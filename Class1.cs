4a72a39ac5443bed15a7d2e21385770deb4538e2
Naylor <NAYLORJ@ethyl.net>
2019-04-04 12:56:46 -0400

    Create extension

--- Class1.cs
+++ Class1.cs
@@ -2,8 +2,29 @@
 
 namespace IsomorphicRouting
 {
-    public class Class1
-    {
 
+    public static class FeRouteExtension {
+
+        /**
+        * Map the front end routes located in a JSON file
+        * For modern SPA applications that do their own routing,
+        * mapping the frontend routes will allow for the backend to
+        * know when to send a proper 404 error 
+        */
+        public static void MapFeRoutes(this IRouteBuilder routeBuilder, string pathToRoutes) {
+
+            // Grab path data from JSON
+            JArray paths = JArray.Parse(File.ReadAllText(pathToRoutes));
+        
+            // Loop through the list of routes and add map them into the routing table
+            foreach (var r in paths.Children<JObject>().Select((x, i) => new { Value = x, Index = i })) {
+                routesBuilder.MapRoute(
+                    name: $"feRoute${r.Index}",
+                    template: (string)r.Value["path"],
+                    defaults: new { controller = "Home", action = "Index" },
+                    constraints: null
+                );  
+            }
+        }
     }
 }
